b716e87d0b07186aea1579dc201eb0e04bc31712
Jennifer Pazos <jmp521@gmail.com>
2019-11-01 23:32:32 -0500

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,67 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(employee) {
+    let emp = {
+        firstName: employee[0], 
+        familyName: employee[1], 
+        title: employee[2], 
+        payPerHour: employee[3], 
+        timeInEvents: [], 
+        timeOutEvents: []
+    }
+    return emp
+}
+
+
+function createEmployeeRecords(array) {
+   return array.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(date) {
+   let event = {
+       type: "TimeIn", 
+       hour: parseInt(date.split(" ")[1], 10), 
+       date: date.split(" ")[0]
+   }
+    this.timeInEvents.push(event)
+    return this
+}
+
+function hoursWorkedOnDate(date) {
+    
+    let timeIn = this.timeInEvents.filter(d => d.date === date)[0]
+    let timeOut = this.timeOutEvents.filter(d => d.date===date)[0]
+    
+
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(date) {
+    // let pay =  this.hoursWorkedOnDate(date).reduce((h, total) => total += h) * this.payPerHour
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+    
+}
+
+function createTimeOutEvent(date) {
+
+    let event = {
+        type: "TimeOut", 
+        hour: parseInt(date.split(" ")[1], 10), 
+        date: date.split(" ")[0]
+    }
+     this.timeOutEvents.push(event)
+     return this
+}
+function findEmployeeByFirstName(collection, firstNameString) {
+    
+    
+    let employee = collection.filter(d => d.firstName === firstNameString)[0]
+    
+    return employee
+
+}
+function calculatePayroll(employees) {
+   return employees.reduce((total, e) => allWagesFor.call(e) + total, 0)
+}
