ca9eeedc5d103d6c5dbea1d5caf95d918c78371c
David Forer <david.j.forer@gmail.com>
2019-11-22 08:59:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeData) {
+  let newEmployee = {};
+  
+  newEmployee.firstName = employeeData[0];
+  newEmployee.familyName = employeeData[1];
+  newEmployee.title = employeeData[2];
+  newEmployee.payPerHour = employeeData[3];
+  newEmployee.timeInEvents = [];
+  newEmployee.timeOutEvents = [];
+  
+  return newEmployee;
+}
+
+function createEmployeeRecords(employeesArray) {
+  return employeesArray.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+    let timeInObj = {
+        type: "TimeIn",
+        hour: (dateStamp.length > 13 ? parseInt(dateStamp.slice(11), 10) : parseInt(dateStamp.slice(9), 10)),
+        date: (dateStamp.length > 13 ? dateStamp.slice(0, 10) : dateStamp.slice(0, 8))
+    }
+    this.timeInEvents.push(timeInObj);
+    return this;
+}
+function createTimeOutEvent(dateStamp) {
+    let timeInObj = {
+        type: "TimeOut",
+        hour: (dateStamp.length > 13 ? parseInt(dateStamp.slice(11), 10) : parseInt(dateStamp.slice(9), 10)),
+        date: (dateStamp.length > 13 ? dateStamp.slice(0, 10) : dateStamp.slice(0, 8))
+    }
+    this.timeOutEvents.push(timeInObj);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find(d => d.date === date);
+    let timeOut = this.timeOutEvents.find(d => d.date === date);
+    let hoursWorked = (timeOut.hour - timeIn.hour) / 100;
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(date) {
+    let wage = hoursWorkedOnDate.call(this,date) * this.payPerHour;
+    return wage;
+}
+
+
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +69,17 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(e => e.firstName === firstName);
+}
+
+function calculatePayroll(srcArray) {
+    let totalWages = srcArray.reduce(function (total, currentValue) {
+        let newTotal = allWagesFor.call(currentValue) + total
+        return newTotal;
+    }, 0)
+    return totalWages;
 }
