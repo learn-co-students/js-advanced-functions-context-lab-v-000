9ebe496f5851a9daca573be361c32ad2a6b52dea
Gingertonic <bethmschofield@gmail.com>
2019-11-29 17:36:58 +0000

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,66 @@ let allWagesFor = function () {
 
     return payable
 }
+
+const createEmployeeRecord = data => {
+    return {
+        firstName: data[0],
+        familyName: data[1],
+        title: data[2],
+        payPerHour: data[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = rows => {
+    return rows.map(createEmployeeRecord)
+}
+
+const createTimeInEvent = function(datetime){
+    this.timeInEvents.push({
+        type: "TimeIn",
+        date: datetime.split(" ")[0],
+        hour: parseInt(datetime.split(" ")[1])
+    })
+    return this
+}
+
+const createTimeOutEvent = function(datetime){
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: datetime.split(" ")[0],
+        hour: parseInt(datetime.split(" ")[1])
+    })
+    return this
+}
+
+
+const hoursWorkedOnDate = function(date){
+    const start = this.timeInEvents.find(d => d.date === date)
+    const end = this.timeOutEvents.find(d => d.date === date)
+    return (end.hour - start.hour) / 100
+}
+
+
+const wagesEarnedOnDate = function(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+// const allWagesFor = function(){
+//     const dates = this.timeInEvents.map(e => e.date)
+//     return dates.reduce((memo, date) => memo + wagesEarnedOnDate.call(this, date), 0)
+// }
+
+// const calculatePayroll = employees => {
+//     return employees.reduce((memo, emp) => memo + allWagesFor(emp), 0)
+// }
+
+const findEmployeeByFirstName = (force, name) => {
+    return force.find(emp => emp.firstName === name)
+}
+
+
+let calculatePayroll = function(employees){
+    return employees.reduce((memo, rec) => memo + allWagesFor.call(rec), 0)
+}
