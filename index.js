f1c27782ccb03b6810ad1397716a3117ed6c6443
nanton83 <nicholas.romans@gmail.com>
2019-12-31 13:01:15 -0500

    Done.

--- index.js
+++ index.js
@@ -16,8 +16,73 @@ let createEmployeeRecords = function(employeeDataArray) {
     }) 
 }
 
+let createTimeInEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
 
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
 
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp){
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(dateToSearch){
+    let clockIn = this.timeInEvents.find(event => event.date === dateToSearch)
+    let clockOut = this.timeOutEvents.find(event => event.date === dateToSearch)
+
+    return (clockOut.hour - clockIn.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateToSearch){
+    let wages = hoursWorkedOnDate.call(this, dateToSearch) * this.payPerHour
+    return wages
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, record){
+        return memo + allWagesFor.call(record)
+    }, 0)
+}
+
+let findEmployeeByFirstName = function(sourceArray, firstName){
+    let firstNameReturn = sourceArray.find(record => record.firstName === firstName)
+    return firstNameReturn
+}
+
+// let findEmployeeByFirstName = function(srcArray, firstName) {
+//     let firstNameReturn =  srcArray.find(record => record.firstName === firstName)
+//     return firstNameReturn
+// }
+
+// let hoursWorkedOnDate = function(employeeObject, dateToSearch) {
+//     console.log(employeeObject)
+//     let clockIn = employeeObject.timeInEvents.find(event => event.date === dateToSearch)
+    
+//     let clockOut = employeeObject.timeOutEvents.find(event => event.date === dateToSearch)
+    
+//     console.log("clockInHour", clockIn.hour)
+//     console.log("clockOutHour", clockOut.hour)
+//     return (clockOut.hour - clockIn.hour) / 100
+// }
+
+// let wagesEarnedOnDate = function(employeeObject, dateToSearch) {
+//     let wages = hoursWorkedOnDate(employeeObject, dateToSearch) * employeeObject.payPerHour
+//     console.log("wages", wages)
+//     return wages
+// }
 // let createTimeInEvent = function(employeeObject, dateStamp) {
 //     console.log(dateStamp)
 //     let [date, hour] = dateStamp.split(' ')
