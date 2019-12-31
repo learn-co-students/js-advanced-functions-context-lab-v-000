14d8ae5229a5160186894da0f89f89197c638f29
nanton83 <nicholas.romans@gmail.com>
2019-12-31 11:57:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 /* Your Code Here */
+let createEmployeeRecord = function(fourElementArray){
+    return {
+        firstName: fourElementArray[0],
+        familyName: fourElementArray[1],
+        title: fourElementArray[2],
+        payPerHour: fourElementArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
+let createEmployeeRecords = function(employeeDataArray) {
+    return employeeDataArray.map(data => {
+        return createEmployeeRecord(data)
+    }) 
+}
+
+
+
+// let createTimeInEvent = function(employeeObject, dateStamp) {
+//     console.log(dateStamp)
+//     let [date, hour] = dateStamp.split(' ')
+//     console.log(date)
+//     console.log(hour)
+//     employeeObject.timeInEvents.push({
+//         type: "TimeIn",
+//         hour: parseInt(hour, 10),
+//         date,
+//     })
+//     return employeeObject
+// }
+
+// let createTimeOutEvent = function(employeeObject, dateStamp) {
+//     let [date, hour] = dateStamp.split(' ')
+
+//     employeeObject.timeOutEvents.push({
+//         type: "TimeOut",
+//         hour: parseInt(hour, 10),
+//         date,
+//     })
+//     return employeeObject
+// }
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
