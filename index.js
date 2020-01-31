5fb9b37e153dee1f445811c08a2baf36f829fe97
dlyons8 <dlyons8@binghamton.edu>
2020-01-31 15:59:54 +0200

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(array) {
+    
+    return {firstName: array[0], familyName: array[1], title: array[2], payPerHour: array[3], timeInEvents: [],
+        timeOutEvents: []}
+}
+
+let createEmployeeRecords= function(array) {
+    return array.map(record => createEmployeeRecord(record))
+}
+
+let createTimeInEvent = function(date) {
+    const info = date.split(' ')
+    this.timeInEvents.push({type: "TimeIn", hour: parseInt(info[1]), date: info[0]})
+    return this
+}
+// updatedBpRecord = createTimeInEvent.call(bpRecord, "2014-02-28 1400")
+
+let createTimeOutEvent = function(date) {
+    const info = date.split(' ')
+    this.timeOutEvents.push({type: "TimeOut", hour: parseInt(info[1]), date: info[0]})
+    return this
+}
+
+let hoursWorkedOnDate = function(date) {
+    let timeOut = this.timeOutEvents.find(element => element.date === date)
+    let timeIn = this.timeInEvents.find(element => element.date === date)
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+let wagesEarnedOnDate = function(date) {
+    let wages = this.payPerHour
+    return hoursWorkedOnDate.call(this, date) * wages;
+}
+
+let calculatePayroll = function(employeeRecords) {
+    return employeeRecords.reduce( (total, element) => allWagesFor.call(element) + total, 0);
+}
+
+let findEmployeeByFirstName = function(employees, firstName) {
+    return employees.find(element => element.firstName === firstName)
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
