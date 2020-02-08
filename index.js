beeae039936cd2dce43e6d9f022657dfb324aa79
daniellecroxton <danikrist@gmail.com>
2020-02-08 11:09:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
-/* Your Code Here */
+let createEmployeeRecord = function(employeeArray) {
+    let employee = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee;
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+let createEmployeeRecords = function(employeesArray) {}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+let createTimeInEvent = function(dateStamp) {}
 
-let allWagesFor = function () {
-    let eligibleDates = this.timeInEvents.map(function (e) {
+let createTimeOutEvent = function(dateStamp) {}
+
+let hoursWorkedOnDate = function(date) {}
+
+let wagesEarnedOnDate = function(date) {}
+
+let allWagesFor = function() {
+    let eligibleDates = this.timeInEvents.map(function(e) {
         return e.date
     })
 
-    let payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    let payable = eligibleDates.reduce(function(memo, d) {
+            return memo + wagesEarnedOnDate.call(this, d)
+        }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
 }
+
+let findEmployeeByFirstName = function(employeeArray, firstName) {}
+
+
+let calculatePayroll = function(employeesArray) {}
