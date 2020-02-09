aa8c886fee9f8105be821a5f23daa7b5e92d4082
daniellecroxton <danikrist@gmail.com>
2020-02-08 21:23:02 -0700

    Done.

--- index.js
+++ index.js
@@ -10,15 +10,48 @@ let createEmployeeRecord = function(employeeArray) {
     return employee;
 }
 
-let createEmployeeRecords = function(employeesArray) {}
+let createEmployeeRecords = function(employeesArray) {
+    return employeesArray.map(function(employee) {
+        return createEmployeeRecord(employee)
+    })
+}
 
-let createTimeInEvent = function(dateStamp) {}
+let createTimeInEvent = function(dateStamp) {
+    let dateSplit = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        date: dateSplit[0],
+        hour: Number(dateSplit[1])
+    })
+    return this;
+}
 
-let createTimeOutEvent = function(dateStamp) {}
+let createTimeOutEvent = function(dateStamp) {
+    let dateSplit = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        date: dateSplit[0],
+        hour: Number(dateSplit[1])
+    })
+    return this;
+}
 
-let hoursWorkedOnDate = function(date) {}
 
-let wagesEarnedOnDate = function(date) {}
+let hoursWorkedOnDate = function(date) {
+    let timeIn = this.timeInEvents.find(function(d) {
+        return d.date === date;
+    })
+    let timeOut = this.timeOutEvents.find(function(d) {
+        return d.date === date;
+    })
+
+    return (timeOut.hour - timeIn.hour) / 100;
+}
+
+let wagesEarnedOnDate = function(date) {
+    let wages = hoursWorkedOnDate.call(this, date) * this.payPerHour;
+    return parseFloat(wages.toString())
+}
 
 let allWagesFor = function() {
     let eligibleDates = this.timeInEvents.map(function(e) {
@@ -32,7 +65,15 @@ let allWagesFor = function() {
     return payable
 }
 
-let findEmployeeByFirstName = function(employeeArray, firstName) {}
+let findEmployeeByFirstName = function(employeesArray, firstName) {
+    return employeesArray.find(function(e) {
+        return e.firstName === firstName;
+    })
+}
 
 
-let calculatePayroll = function(employeesArray) {}
+let calculatePayroll = function(employeesArray) {
+    return employeesArray.reduce(function(memo, employee) {
+        return memo + allWagesFor.call(employee)
+    }, 0)
+}
