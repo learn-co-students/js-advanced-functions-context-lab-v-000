aafe309bad8d04cc62fc7f880ffb1b25ad566093
GiancarloZ <gzabaleta48@gmail.com>
2020-02-10 11:42:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
-/* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]){
+    let employee = {};
+    employee.firstName = firstName
+    employee.familyName = familyName
+    employee.title = title
+    employee.payPerHour = payPerHour
+    employee.timeInEvents = []
+    employee.timeOutEvents = []
+    return employee
+}
 
+function createEmployeeRecords(arr){
+    return arr.map(f => createEmployeeRecord(f))
+}
+ 
+function createTimeInEvent(time){
+    this.timeInEvents.push(Object.assign({}, time, {
+        type: "TimeIn",
+        hour: parseInt(time.split(" ")[1]),
+        date: time.split(" ")[0],
+    }));
+    return this
+}
+ 
+function createTimeOutEvent(time){
+   this.timeOutEvents.push(Object.assign({}, time, {
+        type: "TimeOut",
+        hour: parseInt(time.split(" ")[1]),
+        date: time.split(" ")[0],
+    }));
+    return this
+}
+ 
+function hoursWorkedOnDate(date){
+    let dateWorkedOut = this.timeOutEvents.filter(n => {
+        return n.date === date;
+    });
+    let dateWorkedIn = this.timeInEvents.filter(n => {
+        return n.date === date;
+    });
+ 
+    let hoursWorked = (dateWorkedOut[0].hour - dateWorkedIn[0].hour) / 100;
+    return parseInt(hoursWorked)
+}
+ 
+function wagesEarnedOnDate(date){
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+ 
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -19,4 +66,17 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function calculatePayroll(employees){
+    return employees.reduce(function(total, element){
+        return total + allWagesFor.call(element)
+    }, 0)
+}
+ 
+function findEmployeeByFirstName(arr, str){
+    if (arr.find(e => e.firstName === str)){
+        return arr.find(e => e.firstName === str)
+    }
+    return undefined
 }
