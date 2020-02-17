0ff7db8bfa306aa735e102609d045cd1dc719f13
Bruce Hillsworth <brucehillsworth@Air.local>
2020-02-16 20:15:57 -0700

    Added hoursWorkedOnDate function

--- index.js
+++ index.js
@@ -20,3 +20,45 @@ let allWagesFor = function () {
 
     return payable
 }
+
+
+function createEmployeeRecord(array) {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  };
+};
+
+function createEmployeeRecords(array) {
+  return array.map(e => createEmployeeRecord(e));
+};
+
+function createTimeInEvent(dateTime) {
+  this.timeInEvents.push({
+    type: "TimeIn",
+    date: dateTime.split(" ")[0],
+    hour: parseInt(dateTime.split(" ")[1])});
+  return this;
+};
+
+function createTimeOutEvent(dateTime) {
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    date: dateTime.split(" ")[0],
+    hour: parseInt(dateTime.split(" ")[1])});
+  return this;
+};
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.filter(element => element.date === date).reduce(function(accumulator, currentElement) {
+    return accumulator + currentElement.hour;
+  }, 0);
+  const timeOut = this.timeOutEvents.filter(element => element.date === date).reduce(function(accumulator, currentElement) {
+    return accumulator + currentElement.hour;
+  }, 0);
+  return (timeOut - timeIn) / 100;
+};
