852550c6445295271b4ba106d12d21ae07e192b2
ADogTheWarrior <coolfudge@gmail.com>
2020-09-21 12:13:46 -0600

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,87 @@ let allWagesFor = function () {
 
     return payable
 }
+
+let createEmployeeRecord = function(employeeAry){
+  let employee = {
+    firstName: employeeAry[0],
+    familyName: employeeAry[1],
+    title: employeeAry[2],
+    payPerHour: employeeAry[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+
+  return employee
+}
+
+let createEmployeeRecords = function(aryAry){
+  let employeeRecords = aryAry.map(function (e){
+    return createEmployeeRecord(e)
+  })
+
+  return employeeRecords
+}
+
+let createTimeInEvent = function(date){
+  let timeInEvent = {
+    type: "TimeIn",
+    hour: parseInt(date.split(" ")[1]),
+    date: date.split(" ")[0]
+  }
+  let employee = this
+  employee.timeInEvents.push(timeInEvent)
+
+  return employee
+}
+
+let createTimeOutEvent = function(date){
+  let timeOutEvent = {
+    type: "TimeOut",
+    hour: parseInt(date.split(" ")[1]),
+    date: date.split(" ")[0]
+  }
+  let employee = this
+  employee.timeOutEvents.push(timeOutEvent)
+
+  return employee
+}
+
+let hoursWorkedOnDate = function(date){
+  let timeIn = this.timeInEvents.find(s => s.date === date).hour;
+  let timeOut = this.timeOutEvents.find(s => s.date === date).hour;
+
+  return (timeOut - timeIn)/100;
+}
+
+let wagesEarnedOnDate = function(date){
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+let findEmployeeByFirstName = function(employeeRecords, firstNameString){
+  return employeeRecords.find(employee => employee.firstName == firstNameString);
+}
+
+// Argument(s)
+// Array of employee records
+let calculatePayroll = function(employeeRecords){
+  let payrole = 0
+
+  // employeeRecords.forEach(employee => {
+  //   payrole += allWagesFor(employee)
+  // })
+  // Behavior
+  // Using wagesEarnedOnDate, accumulate the value of all dates worked by the employee in the record used as context. Amount should be returned as a number.
+
+  // Returns
+  // Pay owed for all dates
+  // return payrole
+  return employeeRecords.reduce((payrole, employee) => payrole + allWagesFor(employee))
+}
+
+
+// const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+//
+// // Code your solution here
+//
+// let totalBatteries = batteryBatches.reduce((total, element) => element + total)
